60aeea0ef9989f8632ee2e6e5f35bf33f630428f
antwil <antwilworks@gmail.com>
2021-03-07 20:28:36 -0400

    Added display toggle to evo chain in modal

--- src/screens/pokedex/pokeModal/pokemonModalDisplay.tsx
+++ src/screens/pokedex/pokeModal/pokemonModalDisplay.tsx
@@ -16,24 +16,43 @@ HighchartsMore(Highcharts)
 const EvolutionChainDisplay: React.FC<{
   evolutionChain?: PokeEvolutionChain
 }> = ({ evolutionChain }) => {
+  const [displayState, setDisplayState] = useState({ showChain: false })
   if (!evolutionChain?.chainForms) return null
+  const { showChain } = displayState
+  const setShowChain = (show = true) =>
+    setDisplayState({ ...displayState, showChain: show })
+
   // console.log({ "evolutionChain.chainForms": evolutionChain.chainForms })
   return (
-    <div className="is-flex is-flex-direction-row">
-      {evolutionChain.chainForms.map(
-        (pokem) =>
-          !!pokem?.sprites?.front_default && (
-            <figure
-              key={`Evolution-Chain-Display-${pokem.name}`}
-              className="image is-128x128 mk-center-x"
-            >
-              <img
-                loading="lazy"
-                src={pokem.sprites.front_default}
-                alt={`Evolution-Chain-Display-${pokem.name}`}
-              />
-            </figure>
-          )
+    <div className="is-flex is-flex-direction-row is-align-items-center evolution-chain-display">
+      {!showChain ? (
+        <button
+          className="button is-primary is-light modalButton m-0 px-5 is-size-5"
+          onClick={() => setShowChain()}
+        >
+          Show Evo Chain
+        </button>
+      ) : (
+        <div
+          className="is-flex is-flex-direction-row is-align-items-center"
+          onClick={() => setShowChain(false)}
+        >
+          {evolutionChain.chainForms.map(
+            (pokem) =>
+              !!pokem?.sprites?.front_default && (
+                <figure
+                  key={`Evolution-Chain-Display-${pokem.name}`}
+                  className="image is-128x128 mk-center-x"
+                >
+                  <img
+                    loading="lazy"
+                    src={pokem.sprites.front_default}
+                    alt={`Evolution-Chain-Display-${pokem.name}`}
+                  />
+                </figure>
+              )
+          )}
+        </div>
       )}
     </div>
   )
@@ -74,13 +93,13 @@ const PokemonModalDisplay: React.FC<{
           )}
           <div className="is-flex is-flex-direction-row">
             <button
-              className="button is-primary devolveModalButton m-0 mx-3"
+              className="button is-primary devolveModalButton m-0 mx-4 p-3"
               onClick={() => {}}
             >
               Devolve
             </button>
             <button
-              className="button is-primary evolveModalButton m-0 mx-3"
+              className="button is-primary evolveModalButton m-0 mx-4 p-3"
               onClick={() => {}}
             >
               Evolve
